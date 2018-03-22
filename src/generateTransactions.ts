09c92918e2e5215bfb47ca450875b739a8eeeb1d
Westlake <westlm1@nationwide.com>
2018-03-22 15:16:01 -0400

    cap. debit & credit

--- src/generateTransactions.ts
+++ src/generateTransactions.ts
@@ -19,34 +19,34 @@ export class GenerateTransactions {
         }
         while (date < new Date(2017, 11, 31)) {
             if (date.getDate() == 1 || date.getDate() == 15) {
-                bankAccountTransactions.push(new BankAccountTransaction(bankAccountTransactions.length, bankAccount.id, 'credit', date, pay, 'Paycheck', TransactionTypes.PayCheck))
+                bankAccountTransactions.push(new BankAccountTransaction(bankAccountTransactions.length, bankAccount.id, 'Credit', date, pay, 'Paycheck', TransactionTypes.PayCheck))
             }
             if (date.getDate() == 1) {
                 if (mortgagePayment > 0) {
                     bankAccountTransactions.push(new BankAccountTransaction(
-                        bankAccountTransactions.length, bankAccount.id, 'debit', date, mortgagePayment, 'Mortgage Payment', TransactionTypes.Loan))
+                        bankAccountTransactions.length, bankAccount.id, 'Debit', date, mortgagePayment, 'Mortgage Payment', TransactionTypes.Loan))
                 }
                 else {
                     bankAccountTransactions.push(new BankAccountTransaction(
-                        bankAccountTransactions.length, bankAccount.id, 'debit', date, Math.floor(Math.random()*60000 + 80000), 'Rent Payment', TransactionTypes.Bill))
+                        bankAccountTransactions.length, bankAccount.id, 'Debit', date, Math.floor(Math.random()*60000 + 80000), 'Rent Payment', TransactionTypes.Bill))
                 }
                 if (autoLoanPayment > 0) {
                     bankAccountTransactions.push(new BankAccountTransaction(
-                        bankAccountTransactions.length, bankAccount.id, 'debit', date, autoLoanPayment, 'Auto Loan Payment', TransactionTypes.Loan))
+                        bankAccountTransactions.length, bankAccount.id, 'Debit', date, autoLoanPayment, 'Auto Loan Payment', TransactionTypes.Loan))
                 }
                 bankAccountTransactions.push(new BankAccountTransaction(
-                    bankAccountTransactions.length, bankAccount.id, 'debit', date, Math.floor(Math.random() * 3000 + 9000), 'Water & Sewer', TransactionTypes.Bill))
+                    bankAccountTransactions.length, bankAccount.id, 'Debit', date, Math.floor(Math.random() * 3000 + 9000), 'Water & Sewer', TransactionTypes.Bill))
                 bankAccountTransactions.push(new BankAccountTransaction(
-                    bankAccountTransactions.length, bankAccount.id, 'debit', date, Math.floor(Math.random() * 10000 + 15000), 'Electricity', TransactionTypes.Bill))
+                    bankAccountTransactions.length, bankAccount.id, 'Debit', date, Math.floor(Math.random() * 10000 + 15000), 'Electricity', TransactionTypes.Bill))
                 bankAccountTransactions.push(new BankAccountTransaction(
-                    bankAccountTransactions.length, bankAccount.id, 'debit', date, Math.floor(Math.random() * 14000 + 6000), 'Cable & Internet', TransactionTypes.Bill))
+                    bankAccountTransactions.length, bankAccount.id, 'Debit', date, Math.floor(Math.random() * 14000 + 6000), 'Cable & Internet', TransactionTypes.Bill))
                 bankAccountTransactions.push(new BankAccountTransaction(
-                    bankAccountTransactions.length, bankAccount.id, 'debit', date, Math.floor(Math.random() * 10000 + 5000), 'Cell Phone', TransactionTypes.Bill))
+                    bankAccountTransactions.length, bankAccount.id, 'Debit', date, Math.floor(Math.random() * 10000 + 5000), 'Cell Phone', TransactionTypes.Bill))
             }
             let purchases = Math.floor(Math.random() * 4 + 2)
             for (let i = 1; i <= purchases; i++) {
                 let amount = Math.floor((Math.random() * 6000) + 500)
-                bankAccountTransactions.push(new BankAccountTransaction(bankAccountTransactions.length, bankAccount.id, 'debit', date, amount, 'Debit Card Purchase At {LOCATION} From Card#: XXXXXXXXXXXXXXXX', null))
+                bankAccountTransactions.push(new BankAccountTransaction(bankAccountTransactions.length, bankAccount.id, 'Debit', date, amount, 'Debit Card Purchase At {LOCATION} From Card#: XXXXXXXXXXXXXXXX', null))
             }
             date.setDate(date.getDate() + 1)
         }
